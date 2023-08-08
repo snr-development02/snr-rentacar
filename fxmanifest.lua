ed137bbe2987496abb2dfb4c29b171b1674a9e21
SNR <75149094+snr-development02@users.noreply.github.com>
2023-08-08 09:16:00 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'bodacious' -- SNR STORE | https://discord.gg/TtHFpf3enK
+lua54 'yes'
+game 'gta5'
+
+client_scripts {
+    "script/client.lua",
+    'script/warmenu.lua'
+}
+
+server_scripts {
+    "script/server.lua"
+}
+
+shared_scripts {
+	"shared/config.lua",
+	"shared/locales.lua"
+}
